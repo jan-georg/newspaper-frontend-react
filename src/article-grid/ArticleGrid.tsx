5d8c403a1e836e5305801de599fd6eaa04450cc3
jasper.houben <jasper.houben@codecentric.de>
2024-08-06 10:15:04 +0200

    handled uncaught null exception

--- src/article-grid/ArticleGrid.tsx
+++ src/article-grid/ArticleGrid.tsx
@@ -9,7 +9,7 @@ export function ArticleGird() {
         useNewspaperContext()
 
     const handleAdd = () => {
-        //if(!selectedArticle) return
+        if(!selectedArticle) return
         addArticleToNewspaper(selectedArticle!)
         deselectArticle()
     }
